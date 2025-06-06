eada90fd90a5d58cea91b5338826e65e08143d51
Michal Gromadzki <mgromadzki2001@gmail.com>
2025-06-06 09:33:26 +0200

    add training logs

--- src/scripts/experiments/runner_finetune_per_llm_family.sh
+++ src/scripts/experiments/runner_finetune_per_llm_family.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=per_llmfam      # Job name
-#SBATCH --output=per_llmf6.log           # Standard output log
-#SBATCH --error=per_llmf6.log             # Standard error log
-#SBATCH --time=24:00:00               # Time limit
+#SBATCH --output=per_llmf1.log           # Standard output log
+#SBATCH --error=per_llmf1.log             # Standard error log
+#SBATCH --time=120:00:00               # Time limit
 #SBATCH --gres=gpu:2                  # Request 2 GPUs
 #SBATCH --mem=96G                     # Memory request
 #SBATCH --cpus-per-task=8             # Allocate CPU cores
-#SBATCH --partition=short              # Specify the long queue
+#SBATCH --partition=long              # Specify the long queue
 
 # Ensure pyenv is initialized
-export PATH="$HOME/.pyenv/bin:$PATH"
-export LD_LIBRARY_PATH=$HOME/local/libffi/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$HOME/local/libffi/lib/pkgconfig:$PKG_CONFIG_PATH
-export C_INCLUDE_PATH=$HOME/local/libffi/include:$C_INCLUDE_PATH
+export HOME=/mnt/evafs/faculty/home/mgromadzki
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
 
@@ -23,12 +22,12 @@ eval "$(pyenv virtualenv-init -)"
 # torchrun --nproc_per_node=2 --master_port=29511 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/meta-llama/Llama-3.2-3B-Instruct detect-llama-family 5 8
 
 # Microsoft
-torchrun --nproc_per_node=2 --master_port=29513 training_finetune.py /mnt/evafs/gEroups/re-com/mgromadzki/llms/microsoft/Phi-3-mini-128k-instruct detect-phi-family 5 4
-# torchrun --nproc_per_node=2 --master_port=29513 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-3-small-128k-instruct detect-phi-family 5 2
-# torchrun --nproc_per_node=2 --master_port=29514 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-3-medium-128k-instruct detect-phi-family 5 2
+# torchrun --nproc_per_node=2 --master_port=29513 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-3-mini-128k-instruct detect-phi-family 5 4
+# torchrun --nproc_per_node=2 --master_port=29513 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-3-small-128k-instruct detect-phi-family 5 1
+# torchrun --nproc_per_node=2 --master_port=29554 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-3-medium-128k-instruct detect-phi-family 5 1
 # torchrun --nproc_per_node=2 --master_port=29514 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-3.5-mini-instruct detect-phi-family 5 4
 # torchrun --nproc_per_node=2 --master_port=29515 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/Phi-4-mini-instruct detect-phi-family 5 4
-# torchrun --nproc_per_node=2 --master_port=29514 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/phi-4 detect-phi-4 detect-phi-family 5 2
+torchrun --nproc_per_node=2 --master_port=29555 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/microsoft/phi-4 detect-phi-family 5 1
 
 # Mistral
 # torchrun --nproc_per_node=2 --master_port=29512 training_finetune.py /mnt/evafs/groups/re-com/mgromadzki/llms/mistralai/Mistral-Nemo-Instruct-2407 detect-mistral-family 5 2
